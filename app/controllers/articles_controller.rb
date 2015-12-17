baecb3080001c82d09c51b7d78b18966ae0157e1
railsbeginner <bartosz.dyja@gmail.com>
2015-12-17 21:43:34 +0100

    Add articles CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,30 @@
 class ArticlesController < ApplicationController
   expose(:articles)
+  expose(:article, attributes: :article_params)
+
+  def new
+  end
+
   def show
   end
 
   def index
 
+  end
+
+  def create
+    article.save
+    redirect_to article
+  end
+
+  def update
+    article.save
+    redirect_to article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
