b86251fd8ef7a145d1aa80e7c2f58c7c03df09de
railsbeginner <bartosz.dyja@gmail.com>
2015-12-19 17:29:21 +0100

    Index stylesheets

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,9 @@ module ArticlesHelper
   def formatted_date(date)
     date.strftime("%b %Y")
   end
+
+  def colorize(category)
+    colors = {"reading" => "red", "coding" => "blue"}
+    colors[category]
+  end
 end
