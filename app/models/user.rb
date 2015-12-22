3119820deddd61ffba2e1182dcabf9d63fcce4d6
railsbeginner <bartosz.dyja@gmail.com>
2015-12-22 20:02:02 +0100

    Remove category from #show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
+# User class
 class User < ActiveRecord::Base
   has_many :articles
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-
 end
